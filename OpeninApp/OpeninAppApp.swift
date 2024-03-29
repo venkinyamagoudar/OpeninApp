96ec043eaad50c9930e34ec802dcd3f57998520a
Venkatesh Nyamagoudar <vnyamagoudar@gmail.com>
2024-03-29 11:13:06 +0530

    Initial Commit

---
+++ OpeninApp/OpeninAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpeninAppApp.swift
+//  OpeninApp
+//
+//  Created by Venkatesh Nyamagoudar on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct OpeninAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
